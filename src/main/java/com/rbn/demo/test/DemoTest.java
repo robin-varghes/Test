ae84865c87e8703b8e5fda401a4d24f6b4890bbc
Robin varghese <robin.rvm@gmail.com>
2017-11-03 17:54:40 +0530

    Update DemoTest.java

--- src/main/java/com/rbn/demo/test/DemoTest.java
+++ src/main/java/com/rbn/demo/test/DemoTest.java
@@ -5,6 +5,7 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("sample TEst");
 		System.out.println("sample TEst new");
+		System.out.println("sample TEst new new");
 
 	}
 
