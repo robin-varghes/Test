0cf3197019301ea9dc234c3903100134161954bf
robin varghese <robin.rvm@gmail.com>
2017-11-06 17:40:38 +0530

    new lines from new WorkSpace  3 and 4

--- src/main/java/com/rbn/demo/test/DemoTest.java
+++ src/main/java/com/rbn/demo/test/DemoTest.java
@@ -5,8 +5,8 @@ public class DemoTest {
 	public static void main(String[] args) {
 		System.out.println("sample TEst");
 
-		System.out.println("sample TEst -----  ONE --- 1");
-		System.out.println("sample TEst -----  TWO --- 2");
+		System.out.println("sample TEst -----  THREE --- 3");
+		System.out.println("sample TEst -----  FOUR --- 4");
 		
 		
 
